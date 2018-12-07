b9ae05891d25b51f6bcabcadb686b79a8a8554e3
Rich Castagna <idev.tru@gmail.com>
2018-12-07 15:02:12 -0500

    App Development with Swift, Lesson 3.6 Segues and Navigation Controllers

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
 }
 
